93ace372abd317e1cfef992a084ea786ca67b504
EH <43977335+EthanHaque@users.noreply.github.com>
2020-03-01 13:12:28 -0600

    first commit

---
+++ street-signs/main.py
@@ -0,0 +1,13 @@
+"""
+# learning tensorflow with belgian street signs. Kinda rad
+# Ethan Haque
+# 02/28/2020    
+"""
+
+import tensorflow as tf
+
+# goes through directory and finds all images and labels
+# returns all .ppm and labels in directory 
+# directory: the base directory 
+def loadData(directory):
+    pass
